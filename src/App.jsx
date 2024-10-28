1b19132d709192574b39fe49dcca26ff998a9467
erfan._.teikst <erfan.teist2027@gmail.com>
2024-10-28 22:44:31 +0330

    seting up zustand react-hook-form yup and vazirmatn font

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Button } from './components/ui/button'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <Button variant="outline">Button</Button>
+      <div className="flex justify-center align-middle">
+        <div className="font-vazirmatn text-end bg-slate-600">دکمه</div>
+        <Button className="bg-orange-600 text-white" variant="outline">
+          button
+        </Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
