4798f9c0d69dd0ef670e746156b5d965a5eadf93
erfan._.teikst <erfan.teist2027@gmail.com>
2024-10-28 23:24:04 +0330

    add loading component

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
+import { Loading } from "./components/Loading";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex justify-center align-middle">
-        <div className="font-vazirmatn text-end bg-slate-600">دکمه</div>
+      <div className="flex flex-col justify-center align-middle">
+        <div className="font-vazirmatn font-bold text-end bg-slate-600">
+          دکمه
+        </div>
         <Button className="bg-orange-600 text-white" variant="outline">
           button
         </Button>
+        <Loading />
       </div>
     </>
   );
