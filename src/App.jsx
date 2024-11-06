d5522f7ece590ec38937bbd4ac3fee7a40309a56
erfan._.teikst <erfan.teist2027@gmail.com>
2024-11-07 00:11:22 +0330

    setting axios and fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,47 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { Button } from "./components/ui/button";
 import { Loading } from "./components/Loading";
-import { getData } from "@/Servises/ApiClient";
+import { getData, postData } from "@/Servises/ApiClient";
 
 function App() {
   // const [count, setCount] = useState(0);
 
+  //   useEffect(async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:8000/auth/register/", {
+  //         method: "POST", // Specify the method
+  //         headers: {
+  //           "Content-Type": "application/json", // Set headers
+  //         },
+  //         body: JSON.stringify({
+  //           username: "aliddhjkmosgooltt",
+  //           email: "aldddgfdgvhbkjitt@example.com",
+  //           password: "Ali12345!",
+  //           user_type: "basic",
+  //         }),
+  //       });
+
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! Status: ${response.status}`);
+  //       }
+
+  //       const responseData = await response.json(); // Parse JSON response
+  //       console.log(responseData);
+  //     } catch (error) {
+  //       console.error("Error:", error);
+  //       throw error; // Re-throw to handle it higher up if needed
+  //     }
+  //   }, []);
+
   useEffect(() => {
-    getData().then((data) => console.log(data)); // Fetch data from API and log it in console. This is just a placeholder. Replace it with your actual API call. 3rd argument of useEffect is an array. If it's empty, it will run only once when the component mounts. If it's not empty, it will run every time the array elements change. 4th argument is an optional array of dependencies. If it's provided, it will only run the effect if any of the dependencies change. In this case, it will run every time the data changes. 5th argument is a cleanup function. It will be called when the component unmounts. In this case, it will be called when the component is no longer needed. 6th argument is a dependency array. If it's provided, it will only run the effect if any of the dependencies change. In this case, it will run every time the data changes.
+    const bodyData = {
+      email: "a13w@gmail.com",
+      password: "!A1a2a3a4",
+    };
+
+    postData("/auth/login/", bodyData).then((response) => {
+      console.log("Data posted successfully:", response);
+    });
   }, []);
 
   return (
--- src/Servises/ApiClient/index.js
+++ src/Servises/ApiClient/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://dummyjson.com",
+  baseURL: "http://localhost:8000/",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
