fabba1ef099bf74e96756725daa909bb8065f439
Amin <afiroozi007@gmail.com>
2024-11-07 06:28:56 +0330

    sign up form design & responsiveness improved

--- src/components/Custom/CustomInput/index.jsx
+++ src/components/Custom/CustomInput/index.jsx
@@ -2,37 +2,37 @@ import { Input } from "../../ui/input";
 import styles from "./CustomInput.module.scss";
 
 function CustomInput({
-  value,
-  update,
-  placeholder,
-  autofocus,
-  type,
-  onKey,
-  className,
-  errors,
-  name,
-  register,
+	value,
+	update,
+	placeholder,
+	autofocus,
+	type,
+	onKey,
+	className,
+	errors,
+	name,
+	register,
 }) {
-  // console.log(errors);
-  return (
-    <>
-      <Input
-        // value={value}
-        onChange={(e) => update(e)}
-        type={type}
-        placeholder={placeholder}
-        autoFocus={autofocus}
-        onKeyDown={(e) => onKey(e)}
-        className={`pt-2 text-start min-w-full font-roboto ease-in duration-300 bg-orange-100 mb-2 hover:placeholder:text-bombgray text-bombblack focus:text-bombblack border-solid border-4 border-bombgray focus:border-bomborange focus-visible:ring-0 ${className}`}
-        {...register(name)}
-        name={name}
-        id={name}
-      />
-      {errors[name] && (
-        <p className={styles.error_label}>{errors[name]?.message}</p>
-      )}
-    </>
-  );
+	return (
+		<>
+			<Input
+				// value={value}
+				onChange={(e) => update(e)}
+				type={type}
+				placeholder={placeholder}
+				autoFocus={autofocus}
+				onKeyDown={(e) => onKey(e)}
+				// className={`pt-2 text-start min-w-full font-roboto ease-in duration-300 bg-orange-100 mb-2 hover:placeholder:text-bombgray text-bombblack focus:text-bombblack border-solid border-4 border-bombgray focus:border-bomborange focus-visible:ring-0 ${className}`}
+				className={`pt-2 text-start w-full place-self-start font-roboto ease-in duration-300 bg-orange-100 mb-2 hover:placeholder:text-bombgray text-bombblack focus:text-bombblack border-solid border-4 border-bombgray focus:border-bomborange focus-visible:ring-0 ${className}`}
+				{...register(name)}
+				name={name}
+				id={name}
+			/>
+			{errors[name] ? (
+				<p className={styles.error_label}>{errors[name]?.message}</p>
+			) : <p className="text-xs">f</p>}
+		</>
+	);
 }
 
 export default CustomInput;
--- src/components/Forms/LoginForm/index.jsx
+++ src/components/Forms/LoginForm/index.jsx
@@ -15,70 +15,75 @@ import DrawerButton from "@/components/DrawerButton";
 import { useEffect, useState } from "react";
 
 const schema = yup.object().shape({
-  usernameEmail: yup.string().required("این مورد اجباری است"),
-  password: yup
-    .string()
-    .required("این مورد اجباری است")
-    .min(8, "رمز عبور باید حداقل 8 حرف باشد")
-    .max(50, "رمز عبور طولانی است")
-    .matches(/[A-Z]/, "رمز عبور باید حداقل شامل یک حرف بزرگ انگلیسی باشد")
-    .matches(/[a-z]/, "رمز عبور باید حداقل شامل یک حرف کوچک انگلیسی باشد")
-    .matches(/\d/, "رمز عبور باید حداقل شامل یک عدد باشد")
-    .matches(/[@$!%*?&#]/, "رمز عبور باید حداقل شامل یک علامت باشد"),
+	usernameEmail: yup.string().required("این مورد اجباری است"),
+	password: yup
+		.string()
+		.required("این مورد اجباری است")
+		.min(8, "رمز عبور باید حداقل 8 حرف باشد")
+		.max(50, "رمز عبور طولانی است")
+		.matches(/[A-Z]/, "رمز عبور باید حداقل شامل یک حرف بزرگ انگلیسی باشد")
+		.matches(/[a-z]/, "رمز عبور باید حداقل شامل یک حرف کوچک انگلیسی باشد")
+		.matches(/\d/, "رمز عبور باید حداقل شامل یک عدد باشد")
+		.matches(/[@$!%*?&#]/, "رمز عبور باید حداقل شامل یک علامت باشد"),
 });
 
 function LoginForm() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ resolver: yupResolver(schema) });
-  // const { errors, setErrors } = useState([]);
-  const navigate = useNavigate();
-  const { usernameEmail, password, updateUsernameEmail, updatePassword } =
-    useLoginStore();
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({ resolver: yupResolver(schema) });
+	// const { errors, setErrors } = useState([]);
+	const navigate = useNavigate();
+	const { usernameEmail, password, updateUsernameEmail, updatePassword } =
+		useLoginStore();
 
-  function handleKeyDown(e) {
-    if (e.key === "Enter") {
-      Login(e);
-    }
-  }
-  const onSubmit = (data) => {
-    console.log("Form Submitted", data);
-  };
+	function handleKeyDown(e) {
+		if (e.key === "Enter") {
+			handleSubmit(onSubmit);
+		}
+	}
+	const onSubmit = (data) => {
+		console.log("Form Submitted", data);
+	};
 
-  return (
-    <form className={styles.form_style} onSubmit={handleSubmit(onSubmit)}>
-      <img className={styles.logo} src={logo} alt="logo" />
-      <div className={styles.welcome}>خوش آمدید</div>
-      <div className={styles.text}>برای ورود اطلاعات خود را وارد کنید</div>
-      <Label className={styles.Label}>ایمیل یا نام کاربری</Label>
-      <CustomInput
-        value={usernameEmail}
-        update={(e) => updateUsernameEmail(e.target.value)}
-        placeholder="Email or Username"
-        autofocus={true}
-        onKey={(e) => handleKeyDown(e)}
-        // {...register("usernameEmail")}
-        register={register}
-        name="usernameEmail"
-        errors={errors}
-      />
-      <Label className={styles.Label}>رمز عبور</Label>
-      <PasswordInput
-        value={password}
-        update={(e) => updatePassword(e.target.value)}
-        handleKeyDown={handleKeyDown}
-        errors={errors}
-        register={register}
-        name="password"
-      />
-      <div onClick={() => navigate("/signup")} className={styles.no_account}>
-        حساب کاربری ندارید؟
-      </div>
-      <DrawerButton onClick={handleSubmit(onSubmit)}>ورود</DrawerButton>
-    </form>
-  );
+	return (
+		<form className={styles.form_style} onSubmit={handleSubmit(onSubmit)}>
+			<img className={styles.logo} src={logo} alt="logo" />
+			<div className={styles.welcome}>خوش آمدید</div>
+			<div className={styles.text}>
+				برای ورود اطلاعات خود را وارد کنید
+			</div>
+			<Label className={styles.Label}>ایمیل یا نام کاربری</Label>
+			<CustomInput
+				value={usernameEmail}
+				update={(e) => updateUsernameEmail(e.target.value)}
+				placeholder="Email or Username"
+				autofocus={true}
+				onKey={(e) => handleKeyDown(e)}
+				register={register}
+				name="usernameEmail"
+				errors={errors}
+			/>
+			<Label className={styles.Label}>رمز عبور</Label>
+			<PasswordInput
+				value={password}
+				update={(e) => updatePassword(e.target.value)}
+				placeholder="Password"
+				handleKeyDown={handleKeyDown}
+				errors={errors}
+				register={register}
+				name="password"
+			/>
+			<div
+				onClick={() => navigate("/signup")}
+				className={styles.no_account}
+			>
+				حساب کاربری ندارید؟
+			</div>
+			<DrawerButton onClick={handleSubmit(onSubmit)}>ورود</DrawerButton>
+		</form>
+	);
 }
 
 export default LoginForm;
