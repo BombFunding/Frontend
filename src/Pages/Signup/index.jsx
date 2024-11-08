760384cf004bdcc205714f9356184b6a67dadc91
erfan._.teikst <erfan.teist2027@gmail.com>
2024-11-08 18:13:47 +0330

    make signup page responsive

--- src/Pages/Signup/index.jsx
+++ src/Pages/Signup/index.jsx
@@ -6,9 +6,9 @@ import SignupForm from "@/components/Forms/SignupForm";
 function Signup() {
 	return (
 		<div className={styles.signup_container}>
-			<div className={styles.formstyle}>
+			
 				<SignupForm  />
-			</div>
+
 			{/* <form>nig</form> */}
 			<img className={styles.bg_img} src={loginimage} alt="login" />
 		</div>
