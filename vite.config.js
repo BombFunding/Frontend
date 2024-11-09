68e2ef9ea6f64b27f257707549dce16dafc41459
Amin <afiroozi007@gmail.com>
2024-11-09 14:21:35 +0330

    liara deployment test

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,16 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  server: {
-    port: 3000, // Choose a different port number
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+	server: {
+		port: 3000, // Choose a different port number
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		outDir: "build",
+	},
 });
