cad13dd244ef8c6e4bb205a1133530b1de7d52db
Amin <afiroozi007@gmail.com>
2024-12-03 18:10:51 +0330

    feat(landing): add hero and CTA buttons without functionality

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -9,12 +9,50 @@ import logo7 from "../../assets/logo7.png";
 import logo8 from "../../assets/logo8.png";
 import logo9 from "../../assets/logo9.png";
 import logo10 from "../../assets/logo10.png";
+import shakinghands from "../../assets/shakinghands.png";
+import teamwork1 from "../../assets/teamwork1.png";
 import { Label } from "@radix-ui/react-label";
+import styles from "./Landing.module.scss";
 const Landing = () => {
 	return (
 		<>
+			<div className="flex px-10">
+				<img
+					src={teamwork1}
+					className="w-5/12 place-self-center object-contain"
+				/>
+				<div className="place-items-center place-content-center">
+					<div className="text-black font-vazirmatn text-6xl p-10 text-center align-middle place-self-center">
+						جایی که ایده‌ها زنده می‌شوند
+					</div>
+					<div className="text-black font-vazirmatn text-3xl p-5 text-center align-middle place-self-center">
+						به جامعه‌ای از نوآوران، سرمایه‌گذاران و حمایت‌کنندگان
+						بپیوندید تا دنیای بهتری بسازید
+					</div>
+					<div className="place-self-center flex gap-5">
+						<button className="btn font-vazirmatn text-black bg-bomborange hover:text-white">
+							ایجاد موقعیت جدید
+						</button>
+						<button className="btn font-vazirmatn text-black bg-bomborange hover:text-white">
+							کشف پروژه‌ها
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="flex px-10">
+				<div className="place-items-center place-content-center">
+					<div
+						className={`text-black font-vazirmatn text-6xl p-20 text-center align-middle place-self-center ${styles.bigtext}`}
+					>
+						با ما دسترسی به سرمایه آسان‌تر از همیشه است
+					</div>
+				</div>
+				<img
+					src={shakinghands}
+					className="w-1/2 place-self-center object-contain"
+				/>
+			</div>
 			<div>
-				{/* <img src={Background} className="h-[150vh]" /> */}
 				<Label className="text-black font-vazirmatn text-2xl pr-20 top-10">
 					پربازدیدترینها
 				</Label>
