ab980fdf8509ab3ebcca79d359ad85fd43d7adf1
Amin <afiroozi007@gmail.com>
2024-12-05 18:24:35 +0330

    feat(dashboard): complete personal info section with mockup data

--- src/components/PersonalInfo/PersonalInfo.jsx
+++ src/components/PersonalInfo/PersonalInfo.jsx
@@ -1,6 +1,7 @@
 import headerpic from "../../assets/upProfile.jpg";
 import Likes from "../Likes/Likes";
 import styles from "./PersonalInfo.module.scss";
+import calendarIcon from "../../assets/calendarIcon.png"
 function PersonalInfo({ pfp }) {
 	const x = true;
 	return (
@@ -13,7 +14,7 @@ function PersonalInfo({ pfp }) {
 				src={pfp}
 				className={`${styles.on} absolute place-self-end rounded-full w-[12vw] translate-x-[-5vw] mt-[6vw] border-solid ring-[0.5vw] ring-bomborange`}
 			/>
-			<Likes className="absolute mt-[11vw] ml-[2vw]" />
+			<Likes className="absolute mt-[11.2vw] ml-[1.5vw]" />
 			<section
 				className={`${
 					x ?? "border-solid border-2 border-bomborange "
@@ -28,7 +29,10 @@ function PersonalInfo({ pfp }) {
 						غلامرضا کریمی تبار اصل ترکاشون اصطلخ زیر
 					</h1>
 				</div>
-				<div></div>
+				<div className="flex gap-[0.75vw]">
+                    <img src={calendarIcon} className="h-[2vw]" />
+                    <div className="text-[1.5vw] place-self-center place-content-center align-middle">عضویت از 2024/12/5</div>
+                </div>
 			</section>
 		</>
 	);
