d1fc3203f63aff38469cb27f212b3f0347e0e7b8
Amin <afiroozi007@gmail.com>
2024-12-05 13:01:35 +0330

    feat(dashboard): add comment section and comments

--- src/components/Accounting/Accounting.jsx
+++ src/components/Accounting/Accounting.jsx
@@ -6,7 +6,7 @@ function Accounting() {
 		<section className="border-solid border-2 rounded-lg border-bomborange p-[1.5vw] place-items-center">
 			<h1 className="flex gap-[1vw] p-[3vw]">
 				<img src={Accouting} className="h-[2.75vw] place-self-center" />
-				<div className="h-[3vw] place-self-center">حسابداری</div>
+				<div className="h-[3vw] place-content-center">حسابداری</div>
 			</h1>
 			<div className="p-[2vw] rtl">420000 تومان</div>
 			<button
--- src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
+++ src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
@@ -3,6 +3,7 @@ import { Card } from "@/components/ui/card";
 import PositionBox from "../PositionBox/PositionBox";
 import Accounting from "@/components/Accounting/Accounting";
 import StartupProfiles from "@/components/StartupProfiles/StartupProfiles";
+import CommentSection from "@/components/CommentSection/CommentSection";
 const StartupDashBoard = () => {
 	return (
 		<>
@@ -13,6 +14,7 @@ const StartupDashBoard = () => {
 				{/* <div className={styles.team_row}></div> */}
 				{/* <div className={styles.position_box}>profiles</div> */}
 				<StartupProfiles />
+				<CommentSection />
 				<div className={styles.position_box}>history</div>
 			</Card>
 		</>
