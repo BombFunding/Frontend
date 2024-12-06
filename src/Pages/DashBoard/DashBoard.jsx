323c0f9c93508e33888496584ac3f89390547663
erfan._.teikst <erfan.teist2027@gmail.com>
2024-12-06 13:33:03 +0330

    feat: add multi routing for dashboard

--- src/Pages/DashBoard/DashBoard.jsx
+++ src/Pages/DashBoard/DashBoard.jsx
@@ -1,13 +1,28 @@
+import BaseUserDashBoard from "@/components/DashBoard/BaseUserDashBoard/BaseUserDashBoard";
+import InvestorDashBoard from "@/components/DashBoard/InvestorDashBoard/InvestorDashBoard";
 import StartupDashBoard from "@/components/DashBoard/StartupDashBoard/StartupDashBoard";
+import useTokenStore from "@/stores/TokenStore";
 import React from "react";
+import Error from "../Error/Error";
 // import styles from "./DashBoard";
 
 const DashBoard = () => {
-  return (
-    <>
-      <StartupDashBoard />
-    </>
-  );
+  const userType = useTokenStore((state) => state.userType);
+
+  switch (userType) {
+    case "startup":
+      return <StartupDashBoard />;
+      break;
+    case "investor":
+      return <InvestorDashBoard />;
+      break;
+    case "basic":
+      return <BaseUserDashBoard />;
+      break;
+    default:
+      return <Error />;
+      break;
+  }
 };
 
 export default DashBoard;
--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,6 +1,14 @@
 import PublicCommonProfile from "@/components/Profile/CommonProfile/PublicCommonProfile/PublicCommonProfile";
 
 const Profile = () => {
+	switch (key) {
+		case value:
+			
+			break;
+	
+		default:
+			break;
+	}
 	return (
 		<>
 			<PublicCommonProfile className="w-[85vw]" />
