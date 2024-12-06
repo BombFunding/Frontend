c6620d3ae80007793e7fe050035a1e0e29cfc79b
erfan._.teikst <erfan.teist2027@gmail.com>
2024-12-06 08:55:31 +0330

    fix: integerate with pulled code

--- src/components/Accounting/Accounting.jsx
+++ src/components/Accounting/Accounting.jsx
@@ -1,22 +1,24 @@
 import { useNavigate } from "react-router-dom";
 import Accouting from "../../assets/wallet.png";
-function Accounting() {
-	const Navigate = useNavigate();
-	return (
-		<section className="border-solid border-2 rounded-lg border-bomborange p-[1.5vw] place-items-center">
-			<h1 className="flex gap-[1vw] p-[3vw]">
-				<img src={Accouting} className="h-[2.75vw] place-self-center" />
-				<div className="h-[3vw] place-content-center">حسابداری</div>
-			</h1>
-			<div className="p-[2vw] rtl">420000 تومان</div>
-			<button
-				className="btn bg-bomborange text-black hover:text-white m-[2vw]"
-				onClick={() => Navigate("/ChargeAccount")}
-			>
-				افزایش موجودی
-			</button>
-		</section>
-	);
+function Accounting({ className }) {
+  const Navigate = useNavigate();
+  return (
+    <section
+      className={`border-solid border-2 rounded-lg border-bomborange p-[1.5vw] place-items-center ${className}`}
+    >
+      <h1 className="flex gap-[1vw] p-[3vw]">
+        <img src={Accouting} className="h-[2.75vw] place-self-center" />
+        <div className="h-[3vw] place-content-center">حسابداری</div>
+      </h1>
+      <div className="p-[2vw] rtl">420000 تومان</div>
+      <button
+        className="btn bg-bomborange text-black hover:text-white m-[2vw]"
+        onClick={() => Navigate("/ChargeAccount")}
+      >
+        افزایش موجودی
+      </button>
+    </section>
+  );
 }
 
 export default Accounting;
--- src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
+++ src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
@@ -9,42 +9,50 @@ import { useEffect, useState } from "react";
 import { getData } from "@/Services/ApiClient/Services";
 import useProfileStore from "@/stores/ProfileStore/ProfileStore";
 import TeamBox from "../Sections/TeamBox/TeamBox";
+import { Label } from "@/components/ui/label";
 const StartupDashBoard = () => {
-	const [loading, setLoading] = useState(false);
-	const { setFullname, setUsername, setBio, setAvatar } = useProfileStore();
-	useEffect(() => {
-		setLoading(true);
-		getData("/startup/view_own_startup_profile/").then((data) => {
-			console.log("Startup data: ", data.startup_profile);
-			setFullname(
-				data.startup_profile.first_name +
-					" " +
-					data.startup_profile.last_name
-			);
-			setUsername(data.startup_profile.name);
-			setBio(data.startup_profile.bio);
-			setAvatar(
-				`http://104.168.46.4:8000${data.startup_profile.profile_picture}`
-			);
-			setLoading(false);
-		});
-	}, []);
-	return (
-		<>
-			<Card className={styles.card_style}>
-				<PersonalInfo loading={loading} />
-				<PositionBox />
-				<TeamBox />
-				<Accounting />
-				<div className={styles.position_box}>Team</div>
-				{/* <div className={styles.team_row}></div> */}
-				{/* <div className={styles.position_box}>profiles</div> */}
-				<StartupProfiles />
-				<CommentSection />
-				<div className={styles.position_box}>history</div>
-			</Card>
-		</>
-	);
+  const [loading, setLoading] = useState(false);
+  const { setFullname, setUsername, setBio, setAvatar } = useProfileStore();
+  useEffect(() => {
+    setLoading(true);
+    getData("/startup/view_own_startup_profile/").then((data) => {
+      console.log("Startup data: ", data.startup_profile);
+      setFullname(
+        data.startup_profile.first_name + " " + data.startup_profile.last_name
+      );
+      setUsername(data.startup_profile.name);
+      setBio(data.startup_profile.bio);
+      setAvatar(
+        `http://104.168.46.4:8000${data.startup_profile.profile_picture}`
+      );
+      setLoading(false);
+    });
+  }, []);
+  return (
+    <>
+      <Card className={styles.card_style}>
+        <PersonalInfo loading={loading} />
+        <Label className={styles.label_style}>پوزیشن ها</Label>
+        <PositionBox />
+        <div className="flex flex-row justify-between gap-2 mt-2">
+          <div className="flex flex-col w-2/6 gap-2">
+            <Label className={styles.label_style}>حساب</Label>
+            <Accounting />
+          </div>
+          <div className="flex flex-col w-4/6 gap-2">
+            <Label className={styles.label_style}>اعضاع</Label>
+            <TeamBox />
+          </div>
+        </div>
+        {/* <div className={styles.position_box}>Team</div> */}
+        {/* <div className={styles.team_row}></div> */}
+        {/* <div className={styles.position_box}>profiles</div> */}
+        <StartupProfiles />
+        <CommentSection />
+        <div className={styles.position_box}>history</div>
+      </Card>
+    </>
+  );
 };
 
 export default StartupDashBoard;
