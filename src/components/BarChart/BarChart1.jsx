17eb6035c21241609ec6b87faddbadbbd7ad83d0
AliShahrezaie <Shahrezaieali13@gmail.com>
2024-12-20 02:16:02 +0330

    Connect fund to back-end

--- src/components/BarChart/BarChart1.jsx
+++ src/components/BarChart/BarChart1.jsx
@@ -1,15 +1,18 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import { Area, AreaChart, CartesianGrid, XAxis, Tooltip } from "recharts";
 
-const chartData = [
-  { month: "آبان", fund: 80 },
-  { month: "آذر", fund: 200 },
-  { month: "دی", fund: 120 },
-  { month: "بهمن", fund: 190 },
-  { month: "اسفند", fund: 130 },
-  { month: "اردیبهشت", fund: 140 },
-];
+// const chartData = [
+//   { month: "مهر", fund: 0 },
+//   { month: "آبان", fund: 200 },
+//   { month: "آذر", fund: 120 },
+//   { month: "دی", fund: 190 },
+//   { month: "بهمن", fund: 130 },
+//   { month: "اسفند", fund: 140 },
+// ];
+
+
 
 // Chart configuration
 const chartConfig = {
@@ -41,6 +44,40 @@ const CustomTooltip = ({ active, payload, label }) => {
 };
 
 export function BarChart1() {
+    const [chartData, setChartData] = useState([]); 
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch(
+              "http://104.168.46.4:8000/profile_statics/last-7-days/?username=tara",
+              {
+                method: "GET",
+                headers: {
+                  Accept: "application/json",
+                  "X-CSRFTOKEN": "51VoasHbfJ0uJb2wtmNJ08Qg7RYYBDZRZGSmZ4u3eZ0kTq0DjcLGlebc8HlYPLle", // Add CSRF token here
+                },
+              }
+            );
+    
+            if (!response.ok) {
+              throw new Error(`HTTP error! status: ${response.status}`);
+            }
+    
+            const data = await response.json(); // Parse response JSON
+            setChartData(data); // Update chart data state
+          } catch (error) {
+            console.error("Failed to fetch data:", error);
+          } finally {
+            setLoading(false); // Set loading to false after fetching data
+          }
+        };
+    
+        fetchData();
+      }, []);
+      if (loading) {
+        return <div>Loading chart...</div>;
+      }    
   return (
     <div className="p-6 bg-white rounded-lg shadow-md">
       {/* Chart */}
