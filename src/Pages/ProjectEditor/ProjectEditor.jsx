d3e317ab66aeb77d4dfda1d12dc72a410ba8420d
erfan._.teikst <erfan.teist2027@gmail.com>
2024-12-25 17:05:43 +0330

    feat: add ability to import svg as a react component

--- src/Pages/ProjectEditor/ProjectEditor.jsx
+++ src/Pages/ProjectEditor/ProjectEditor.jsx
@@ -1,12 +1,14 @@
 import Editor from "@/components/Editor/Editor";
 import React from "react";
 import { useParams } from "react-router-dom";
+import PicHolder from "@/assets/picHolder.svg?react";
 
 const ProjectEditor = () => {
   const { projectId } = useParams();
   return (
     <>
       <div className="text-3xl p-8 ">ویرایشگر</div>
+      <PicHolder className="w-[100%] h-auto" />
       <Editor id={projectId} />
     </>
   );
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
-	server: {
-		port: 3000, // Choose a different port number
-	},
-	plugins: [react()],
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./src"),
-		},
-	},
-	build: {
-		outDir: "build",
-	},
+  server: {
+    port: 3000, // Choose a different port number
+  },
+  plugins: [svgr(), react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    outDir: "build",
+  },
 });
