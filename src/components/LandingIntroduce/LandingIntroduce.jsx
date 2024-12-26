89f1a4d8ec34d583457e33cd380932ee81737686
taraneh <taraneh.abdellahi2@gmail.com>
2024-12-26 05:34:15 +0330

    add bomb funding title

--- src/components/LandingIntroduce/LandingIntroduce.jsx
+++ src/components/LandingIntroduce/LandingIntroduce.jsx
@@ -1,4 +1,3 @@
-import React, { useEffect } from 'react';
 import Lenis from 'lenis';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
@@ -13,8 +12,96 @@ import img8 from "../../assets/landing8.png";
 import img9 from "../../assets/landing9.jpg";
 import BarChart from './BarChart';
 import FlipAnimation from "@/components/LandingIntroduce/FlipAnimation";
+import { motion, useInView, useAnimation } from "framer-motion";
+import { useEffect, useRef } from "react";
+
 
 gsap.registerPlugin(ScrollTrigger);
+
+
+const AnimatedText = ({
+  text,
+  el: Wrapper = "p",
+  className,
+  once,
+  repeatDelay,
+  animation = defaultAnimations,
+}) => {
+  const controls = useAnimation();
+  const textArray = Array.isArray(text) ? text : [text];
+  const ref = useRef(null);
+  const isInView = useInView(ref, { amount: 0.5, once });
+
+  useEffect(() => {
+    let timeout;
+    const show = () => {
+      controls.start("visible");
+      if (repeatDelay) {
+        timeout = setTimeout(async () => {
+          await controls.start("hidden");
+          controls.start("visible");
+        }, repeatDelay);
+      }
+    };
+
+    if (isInView) {
+      show();
+    } else {
+      controls.start("hidden");
+    }
+
+    return () => clearTimeout(timeout);
+  }, [isInView]);
+
+  return (
+    <Wrapper className={className}>
+      <span className="sr-only">{textArray.join(" ")}</span>
+      <motion.span
+        ref={ref}
+        initial="hidden"
+        animate={controls}
+        variants={{
+          visible: { transition: { staggerChildren: 0.1 } },
+          hidden: {},
+        }}
+        aria-hidden
+      >
+        {textArray.map((line, lineIndex) => (
+          <span className="block" key={`${line}-${lineIndex}`}>
+            {line.split(" ").map((word, wordIndex) => (
+              <span className="inline-block" key={`${word}-${wordIndex}`}>
+                {word.split("").map((char, charIndex) => (
+                  <motion.span
+                    key={`${char}-${charIndex}`}
+                    className="inline-block"
+                    variants={animation}
+                  >
+                    {char}
+                  </motion.span>
+                ))}
+                <span className="inline-block">&nbsp;</span>
+              </span>
+            ))}
+          </span>
+        ))}
+      </motion.span>
+    </Wrapper>
+  );
+};
+
+const defaultAnimations = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.1,
+    },
+  },
+};
 
 const App = () => {
   useEffect(() => {
@@ -179,6 +266,14 @@ const App = () => {
           />
         </div>
         <div className={styles.content}>
+          <AnimatedText
+          el="h2"
+          text={[
+            "BOMB",
+            "FUNDING"
+          ]}
+          repeatDelay={5000}
+        />
           <h1 className={`${styles.title} font-vazirmatn text-[3vw] text-right`}>
         <FlipAnimation/>
           </h1>
