567f0787e90ec8e8e076cb72eb7f32b1bf323da9
AliRwxza <alireza.rahimi1328@gmail.com>
2024-12-27 19:07:17 +0330

    feat: added subcategories and hrefs to each category

--- src/components/NavbarDropDownSCN/NavbarDropDownSCN.jsx
+++ src/components/NavbarDropDownSCN/NavbarDropDownSCN.jsx
@@ -19,37 +19,63 @@ import { FiChevronDown } from "react-icons/fi";
 
 const components = [
   {
-    title: "category1",
-    href: "/docs/primitives/alert-dialog",
+    title: "تکنولوژی",
+    subs: {
+      "هوش مصنوعی": "/ai",
+      "اینترنت اشیا": "/iot",
+      امنیت: "/security",
+      "نرم افزار": "/software",
+      "واقعیت افزوده": "/ar",
+    },
     description:
       "A modal dialog that interrupts the user with important content and expects a response.",
   },
   {
-    title: "category2",
-    href: "/docs/primitives/hover-card",
+    title: "گردشگری",
+    subs: {
+      فرهنگی: "/cultural",
+      "بین المللی": "/international",
+      شهری: "/urban",
+    },
     description:
       "For sighted users to preview content available behind a link.",
   },
   {
-    title: "category3",
-    href: "/docs/primitives/progress",
+    title: "آموزشی",
+    subs: {
+      "کتاب و نشریات": "/books",
+      "توسعه فردی": "/self-development",
+      آموزشگاه: "/academy",
+    },
     description:
       "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
   },
   {
-    title: "category4",
-    href: "/docs/primitives/scroll-area",
+    title: "مالی",
+    subs: {
+      "ارز دیجیتال": "/financial",
+      بیمه: "/financial",
+      "صندوق سرمایه گذاری": "/financial",
+    },
     description: "Visually or semantically separates content.",
   },
   {
-    title: "category5",
-    href: "/docs/primitives/tabs",
+    title: "هنری",
+    subs: {
+      سینما: "/arts",
+      موسیقی: "/arts",
+      "صنایع دستی": "/arts",
+    },
     description:
       "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
   },
   {
-    title: "category6",
-    href: "/docs/primitives/tooltip",
+    title: "سلامتی",
+    subs: {
+      تغذیه: "/health",
+      "سلامت روان": "/health",
+      درمان: "/health",
+    },
     description:
       "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
   },
@@ -94,25 +120,36 @@ function NavbarDropDownSCN() {
     <div className="w-screen -translate-y-2 h-7 grid grid-cols-6 px-[9vw] gap-[1.5vw] sm:text-[1.6vw] md:text-[1.5vw] lg:text-[1.2vw]">
       {components.map((comp, i) => {
         return (
-          <div key={i}>
-            <div
-              className="hover:cursor-pointer h-fit ml-[2.5vw] px-[1vw] w-[12vw] flex flex-row text-black"
-              onMouseEnter={() => setSelected(i)}
-              onMouseLeave={() => setSelected(null)}
-            >
+          <div
+            key={i}
+            className="place-items-center"
+            onMouseEnter={() => setSelected(i)}
+            onMouseLeave={() => setSelected(null)}
+          >
+            <div className="hover:cursor-pointer place-content-center h-fit ml-[2.5vw] px-[1vw] w-[12vw] flex  flex-row text-black">
               {comp.title}
               <FiChevronDown
-                className={`mt-1.5 ml-[0.2vw] transition duration-400 ${
-                  selected == i ? "rotate-180" : ""
+                className={`mt-1.5 ml-[0.2vw] transition ${
+                  selected == i ? "rotate-180  duration-400" : ""
                 }`}
               />
             </div>
             <div
-              className={`h-fit p-2 overflow-hidden text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
-                selected == i ? "" : "hidden"
+              className={`p-2 container rtl overflow-hidden grid text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
+                selected === i
+                  ? "grid-cols-2 gap-2 auto-rows-auto text-[1vw]"
+                  : "hidden"
               }`}
             >
-              {comp.description}
+              {Object.entries(comp.subs).map(([label, link], index) => (
+                <a
+                  key={index}
+                  href={link}
+                  className="text-white hover:bg-gray-700 p-2 rounded-md"
+                >
+                  {label}
+                </a>
+              ))}
             </div>
           </div>
         );
