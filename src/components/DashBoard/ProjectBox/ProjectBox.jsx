d3b4f3e4fa97ac78028e64ac0ea10e122d552932
erfan._.teikst <erfan.teist2027@gmail.com>
2024-12-28 14:39:22 +0330

    fix: dashboard projectBox feat: add projectStore

--- src/components/DashBoard/ProjectBox/ProjectBox.jsx
+++ src/components/DashBoard/ProjectBox/ProjectBox.jsx
@@ -1,25 +1,53 @@
 import styles from "./ProjectBox.module.scss";
 import ProjectItem from "../ProjectItem/ProjectItem";
 import EmptySection from "@/components/EmptySection/EmptySection";
+import useProjectStore from "@/stores/ProjectStore/ProjectStore";
+import { use, useEffect, useState } from "react";
+import { getData } from "@/Services/ApiClient/Services";
+import { Loading } from "@/components/Loading/Loading";
 
-const ProjectBox = ({ projects, className, type }) => {
-	return (
-		<div className={`${className} ${styles.box}`}>
-			{projects?.length > 0 ? (
-				<div className={styles.project_list}>
-					{projects?.map((project, index) => (
-						<ProjectItem
-							header={project.header}
-							name={project.name}
-							key={index}
-						/>
-					))}
-				</div>
-			) : (
-				<EmptySection type={type} />
-			)}
-		</div>
-	);
+const ProjectBox = ({ className, type }) => {
+  const [loading, setLoading] = useState(false);
+  const [projects, setProjects] = useState(null);
+  const [updateProjects, setUpdateProjects] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    getData("/projects/").then((data) => {
+      console.log("projects:", data);
+      setProjects(data);
+      setLoading(false);
+    });
+  }, [updateProjects]);
+  if (loading) {
+    return (
+      <div className={`${className} ${styles.box}`}>
+        <Loading />
+      </div>
+    );
+  }
+  return (
+    <div className={`${className} ${styles.box}`}>
+      {projects?.length > 0 ? (
+        <div className={styles.project_list}>
+          {projects?.map((project, index) => (
+            <ProjectItem
+              header={project.header}
+              name={project.name}
+              key={index}
+            />
+          ))}
+        </div>
+      ) : (
+        <EmptySection type={type} />
+      )}
+      <button
+        className="btn bg-bomborange text-white h-8"
+        style={{ "min-height": "0px" }}
+      >
+        ساخت پروژه جدید
+      </button>
+    </div>
+  );
 };
 
 export default ProjectBox;
