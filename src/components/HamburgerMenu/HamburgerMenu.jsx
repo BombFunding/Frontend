d0a52ea045c3219eb0a00557f7087b86b0acb000
AliRwxza <117801976+AliRwxza@users.noreply.github.com>
2025-01-02 09:20:32 +0330

    Fix/everything (#48)
    * refactor: made a responsive navbar dropdown from scratch
    
    * feat: added subcategories and hrefs to each category
    
    * refactor: made hamburger-menu items as one
    
    * feat: add profile box & logout to hamburger menu
    
    * feat: add about us to hamburger menu
    
    * refactor: change color of hamburger icon
    
    * fix: swap logout and about-us buttons in hamurger menu
    
    * feat: add share button which copies the url of the page on click, also added a custom toast to notify the user
    
    * refactor: move the toast to bottom-center of the screen

--- src/components/HamburgerMenu/HamburgerMenu.jsx
+++ src/components/HamburgerMenu/HamburgerMenu.jsx
@@ -1,5 +1,4 @@
-import { useClickAway } from "react-use";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { Squash as Hamburger } from "hamburger-react";
 // import { routes } from "../../routes/routes";
@@ -8,19 +7,17 @@ import { FiChevronDown, FiSearch } from "react-icons/fi";
 import { CgProfile } from "react-icons/cg";
 import { GrTechnology } from "react-icons/gr";
 import { IoMdBrush } from "react-icons/io";
-import { BsHeartPulse } from "react-icons/bs";
+import { BsHeartPulse, BsInfoCircle } from "react-icons/bs";
 import { LuPlane } from "react-icons/lu";
 import { FaBookOpen } from "react-icons/fa";
 import { FaCoins } from "react-icons/fa";
-import { SlLogin } from "react-icons/sl";
-
+import { SlLogin, SlLogout } from "react-icons/sl";
+import useProfileStore from "@/stores/ProfileStore/ProfileStore";
+import { getData } from "@/Services/ApiClient/Services";
+import useTokenStore from "@/stores/TokenStore";
+// import { pic } from "../../assets/defaultpfp.png";
 function HamburgerMenu({ isOpen, setOpen, mode, token }) {
   const routes = [
-    {
-      title: "خانه",
-      href: "/",
-      Icon: BiHomeAlt2,
-    },
     token
       ? {
           title: "پروفایل",
@@ -33,52 +30,98 @@ function HamburgerMenu({ isOpen, setOpen, mode, token }) {
           Icon: SlLogin,
         },
     {
+      title: "خانه",
+      href: "/",
+      Icon: BiHomeAlt2,
+    },
+    {
       title: "جستجو",
       href: "",
       Icon: FiSearch,
     },
-  ];
-
-  const categories = [
     {
-      title: "تکنولوژی",
-      href: "/",
-      Icon: GrTechnology,
+      title: "دسته بندی ها",
+      href: "",
+      Icon: FiChevronDown,
+      categories: [
+        {
+          title: "تکنولوژی",
+          href: "/",
+          Icon: GrTechnology,
+        },
+        {
+          title: "هنری",
+          href: "/",
+          Icon: IoMdBrush,
+        },
+        {
+          title: "سلامت",
+          href: "/",
+          Icon: BsHeartPulse,
+        },
+        {
+          title: "گردشگری",
+          href: "/",
+          Icon: LuPlane,
+        },
+        {
+          title: "آموزشی",
+          href: "/",
+          Icon: FaBookOpen,
+        },
+        {
+          title: "مالی",
+          href: "/",
+          Icon: FaCoins,
+        },
+      ],
     },
     {
-      title: "هنری",
-      href: "/",
-      Icon: IoMdBrush,
+      title: "درباره ما",
+      href: "/aboutus",
+      Icon: BsInfoCircle,
     },
     {
-      title: "سلامت",
-      href: "/",
-      Icon: BsHeartPulse,
-    },
-    {
-      title: "گردشگری",
-      href: "/",
-      Icon: LuPlane,
-    },
-    {
-      title: "آموزشی",
-      href: "/",
-      Icon: FaBookOpen,
-    },
-    {
-      title: "مالی",
-      href: "/",
-      Icon: FaCoins,
+      title: "خروج",
+      href: "#",
+      Icon: SlLogout,
     },
   ];
   // const [isOpen, setOpen] = useState(false);
   const ref = useRef(null);
   const [showCategories, toggleShowCategories] = useState(false);
-  console.log(showCategories);
+  const [userdata, setUserdata] = useState({ username: "", fullname: "" });
+  const [color, setColor] = useState("black");
+  const { deleteToken } = useTokenStore();
+  const { avatar } = useProfileStore();
+
+  useEffect(() => {
+    getData(`/auth/view_own_baseuser_profile/`).then((data) => {
+      setUserdata({
+        username: data.base_profile.name,
+        fullname:
+          data.base_profile.first_name + " " + data.base_profile.last_name,
+      });
+    });
+  }, []);
+
+  const handleToggle = (value) => {
+    setOpen(value);
+    if (value === true) {
+      setColor("gray");
+    } else {
+      setColor("black");
+    }
+  };
 
   return (
-    <div ref={ref} className={mode}>
-      <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+    <div ref={ref} className={`${mode}`}>
+      <Hamburger
+        toggled={isOpen}
+        size={20}
+        toggle={handleToggle}
+        color={color}
+      />
       <AnimatePresence>
         {isOpen && (
           <motion.div
@@ -86,99 +129,106 @@ function HamburgerMenu({ isOpen, setOpen, mode, token }) {
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
             transition={{ duration: 0.2 }}
-            // className="fixed left-0 shadow-4xl right-0 top-[3.5rem] p-5 pt-0 bg-neutral-950 border-b border-b-white/20"
-            className="fixed left-0 shadow-4xl right-0 top-[3rem] p-5 overflow-hidden overflow-y-scroll pt-0 bg-neutral-950 border-b border-b-white/20"
+            className="fixed left-0 shadow-4xl h-screen right-0 p-5 overflow-hidden overflow-y-scroll pt-0 bg-neutral-950 border-b border-b-white/20"
           >
             <ul className="grid gap-2">
               {routes.map((route, idx) => {
                 const { Icon } = route;
-
+                if (route.title === "خروج" && !token) {
+                  return null;
+                }
                 return (
                   <motion.li
                     initial={{ scale: 0, opacity: 0 }}
                     animate={{ scale: 1, opacity: 1 }}
+                    key={idx}
                     transition={{
                       type: "spring",
                       stiffness: 260,
                       damping: 20,
                       delay: 0.1 + idx / 10,
                     }}
-                    key={route.title}
-                    className="w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-800 via-neutral-950 to-neutral-700"
+                    className={`w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-700 via-neutral-950 ${
+                      !showCategories ? "to-neutral-700" : "to-neutral-950"
+                    }`}
                   >
                     <a
-                      onClick={() => setOpen((prev) => !prev)}
-                      className={
-                        "flex items-center justify-between w-full p-5 rounded-xl bg-neutral-950"
-                      }
+                      onClick={(e) => {
+                        if (route.categories) {
+                          e.preventDefault(); // Prevent navigation
+                          toggleShowCategories((prev) => !prev);
+                        } else if (route.title === "خروج") {
+                          deleteToken();
+                        }
+                      }}
                       href={route.href}
+                      className={`flex items-center justify-between w-full p-5 ${
+                        showCategories ? "" : "rounded-xl"
+                      } bg-neutral-950`}
                     >
-                      <Icon className="text-xl" />
-                      <span className="flex gap-1 text-lg">{route.title}</span>
+                      {route.categories ? (
+                        <Icon
+                          className={`transition-transform duration-300 ${
+                            showCategories ? "rotate-180" : ""
+                          }`}
+                        />
+                      ) : route.title !== "پروفایل" ? (
+                        <Icon className="text-xl" />
+                      ) : (
+                        <div className="w-[90px] rounded-full overflow-hidden border-solid border-2 border-bomborange">
+                          <img
+                            className="object-cover p-[0.125rem] rounded-full"
+                            src={avatar}
+                          />
+                        </div>
+                      )}
+                      {route.title !== "پروفایل" ? (
+                        <span className="flex gap-1 text-lg">
+                          {route.title}
+                        </span>
+                      ) : (
+                        <div className="flex flex-col gap-2">
+                          <div className="font-extrabold text-[4.5vw]">
+                            {userdata.fullname}
+                          </div>
+                          <div className="text-[2vw]">@{userdata.username}</div>
+                        </div>
+                      )}
                     </a>
+                    {showCategories &&
+                      route.categories &&
+                      route.categories.map((category, i) => {
+                        const { Icon } = category;
+                        return (
+                          <motion.li
+                            initial={{ scale: 0, opacity: 0 }}
+                            animate={{ scale: 1, opacity: 1 }}
+                            transition={{
+                              type: "spring",
+                              stiffness: 260,
+                              damping: 20,
+                            }}
+                            key={i}
+                            className="px-3 text-[#7e8089]"
+                          >
+                            <a
+                              onClick={() => setOpen(false)}
+                              className={
+                                "flex justify-between w-full p-1 bg-inherit"
+                              }
+                              href={category.href}
+                            >
+                              <Icon className="text-lg" />
+                              <span className="flex gap-1 text-md">
+                                {category.title}
+                              </span>
+                            </a>
+                          </motion.li>
+                        );
+                      })}
                   </motion.li>
                 );
               })}
-              <motion.li
-                initial={{ scale: 0, opacity: 0 }}
-                animate={{ scale: 1, opacity: 1 }}
-                transition={{
-                  type: "spring",
-                  stiffness: 260,
-                  damping: 20,
-                  delay: 0.1 + routes.length / 10,
-                }}
-                className={`h-fit w-full p-[0.08rem] rounded-xl bg-gradient-to-tr from-neutral-700 via-neutral-950 ${
-                  !showCategories ? "to-neutral-700" : "to-neutral-950"
-                }`}
-              >
-                <a
-                  onClick={() => toggleShowCategories(() => !showCategories)}
-                  className={`flex items-center justify-between w-full p-5 ${
-                    showCategories ? "" : "rounded-xl"
-                  } bg-neutral-950`}
-                >
-                  {/* <Icon className="text-xl" /> */}
-                  <FiChevronDown
-                    className={`transition-transform ${
-                      showCategories ? "rotate-180" : ""
-                    }`}
-                  />
-                  <span className="flex gap-1 text-lg">{"دسته بندی ها"}</span>
-                </a>
-                {showCategories &&
-                  categories.map((category, i) => {
-                    const { Icon } = category;
-                    return (
-                      <motion.li
-                        initial={{ scale: 0, opacity: 0 }}
-                        animate={{ scale: 1, opacity: 1 }}
-                        transition={{
-                          type: "spring",
-                          stiffness: 260,
-                          damping: 20,
-                          // delay: 0.1 + (5 + i) / 10,
-                        }}
-                        key={i}
-                        className="px-3 text-[#7e8089]"
-                      >
-                        <a
-                          onClick={() => setOpen((prev) => !prev)}
-                          className={
-                            "flex justify-between w-full p-1 bg-inherit"
-                          }
-                          href={category.href}
-                        >
-                          <Icon className="text-lg" />
-
-                          <span className="flex gap-1 text-md">
-                            {category.title}
-                          </span>
-                        </a>
-                      </motion.li>
-                    );
-                  })}
-              </motion.li>
             </ul>
           </motion.div>
         )}
---
+++ src/components/NavbarDropDownSCN/NavbarDropDownSCN.jsx
@@ -0,0 +1,161 @@
+"use client";
+
+import * as React from "react";
+// import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+// import { Icons } from "@/components/icons";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+// import { Link } from "radix-ui";
+import { FiChevronDown } from "react-icons/fi";
+
+const components = [
+  {
+    title: "تکنولوژی",
+    subs: {
+      "هوش مصنوعی": "/ai",
+      "اینترنت اشیا": "/iot",
+      امنیت: "/security",
+      "نرم افزار": "/software",
+      "واقعیت افزوده": "/ar",
+    },
+    description:
+      "A modal dialog that interrupts the user with important content and expects a response.",
+  },
+  {
+    title: "گردشگری",
+    subs: {
+      فرهنگی: "/cultural",
+      "بین المللی": "/international",
+      شهری: "/urban",
+    },
+    description:
+      "For sighted users to preview content available behind a link.",
+  },
+  {
+    title: "آموزشی",
+    subs: {
+      "کتاب و نشریات": "/books",
+      "توسعه فردی": "/self-development",
+      آموزشگاه: "/academy",
+    },
+    description:
+      "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+  },
+  {
+    title: "مالی",
+    subs: {
+      "ارز دیجیتال": "/financial",
+      بیمه: "/financial",
+      "سرمایه گذاری": "/financial",
+    },
+    description: "Visually or semantically separates content.",
+  },
+  {
+    title: "هنری",
+    subs: {
+      سینما: "/arts",
+      موسیقی: "/arts",
+      "صنایع دستی": "/arts",
+    },
+    description:
+      "A set of layered sections of content—known as tab panels—that are displayed one at a time.",
+  },
+  {
+    title: "سلامتی",
+    subs: {
+      تغذیه: "/health",
+      "سلامت روان": "/health",
+      درمان: "/health",
+    },
+    description:
+      "A popup that displays information related to an element when the element receives keyboard focus or the mouse hovers over it.",
+  },
+];
+
+function NavbarDropDownSCN() {
+  const [selected, setSelected] = React.useState(null);
+  //   console.log(components);
+  return (
+    // <NavigationMenu className="translate-y-[-1.5vw] bg-transparent">
+    //   <NavigationMenuList className="left-40">
+    //     {components.map((comp, i) => {
+    //       return (
+    //         <NavigationMenuItem key={i} className="">
+    //           <NavigationMenuTrigger className="bg-transparent text-black">
+    //             {comp.title}
+    //           </NavigationMenuTrigger>
+    //           <NavigationMenuContent className={`ml-40`}>
+    //             <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px]">
+    //               <li className="row-span-3">
+    //                 <NavigationMenuLink asChild>
+    //                   <a
+    //                     className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+    //                     href={comp.href}
+    //                   >
+    //                     <div className="mb-2 mt-4 text-lg font-medium">
+    //                       {comp.title}
+    //                     </div>
+    //                     <p className="text-sm leading-tight text-muted-foreground">
+    //                       {comp.description}
+    //                     </p>
+    //                   </a>
+    //                 </NavigationMenuLink>
+    //               </li>
+    //             </ul>
+    //           </NavigationMenuContent>
+    //         </NavigationMenuItem>
+    //       );
+    //     })}
+    //   </NavigationMenuList>
+    // </NavigationMenu>
+    <div className="w-screen -translate-y-2 h-7 mt-6 grid grid-cols-6 px-[9vw] gap-[1.5vw] sm:text-[1.6vw] md:text-[1.5vw] lg:text-[1.2vw] font-vazirmatn">
+      {components.map((comp, i) => {
+        return (
+          <div
+            key={i}
+            className="place-items-center"
+            onMouseEnter={() => setSelected(i)}
+            onMouseLeave={() => setSelected(null)}
+          >
+            <div className="hover:cursor-pointer place-content-center h-fit ml-[2.5vw] px-[1vw] w-[12vw] flex  flex-row text-black">
+              {comp.title}
+              <FiChevronDown
+                className={`mt-1.5 ml-[0.2vw] transition ${
+                  selected == i ? "rotate-180  duration-300" : ""
+                }`}
+              />
+            </div>
+            <div
+              className={`p-2 container rtl overflow-hidden grid text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
+                selected === i
+                  ? "grid-cols-2 gap-2 auto-rows-auto text-[1vw]"
+                  : "hidden"
+              }`}
+            >
+              {Object.entries(comp.subs).map(([label, link], index) => (
+                <a
+                  key={index}
+                  href={link}
+                  className="text-white hover:bg-gray-700 p-2 rounded-md"
+                >
+                  {label}
+                </a>
+              ))}
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default NavbarDropDownSCN;
