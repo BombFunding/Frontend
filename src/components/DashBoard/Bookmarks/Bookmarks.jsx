e0e7ff7255f2b3b46baece7ef10d0fe4e0862cf0
Amin <afiroozi007@gmail.com>
2025-01-03 15:18:40 +0330

    feat(startup-dashboard): add bookmarked projects to dashboard

---
+++ src/components/DashBoard/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,51 @@
+import ProjectItem from "../ProjectItem/ProjectItem";
+import EmptySection from "@/components/EmptySection/EmptySection";
+import { useEffect, useState } from "react";
+import { getData } from "@/Services/ApiClient/Services";
+import { Loading } from "@/components/Loading/Loading";
+import { useNavigate } from "react-router-dom";
+import styles from "./Bookmarks.module.scss";
+const Bookmarks = ({ className, type }) => {
+	const Navigate = useNavigate();
+	const [loading, setLoading] = useState(false);
+	const [projects, setProjects] = useState([]);
+	useEffect(() => {
+		setLoading(true);
+		getData(`/bookmark/`).then((data) => {
+			console.log("bookmark", data);
+			setProjects(data);
+			setLoading(false);
+		});
+	}, []);
+	if (loading) {
+		return (
+			<div className={`${className} ${styles.box}`}>
+				<Loading />
+			</div>
+		);
+	}
+	return (
+		<div className={`${className} ${styles.box}`}>
+			{projects?.length > 0 ? (
+				<div className={styles.project_list}>
+					{projects?.map((project, index) => (
+						<ProjectItem
+							header={`http://104.168.46.4:8000${project.project_header_picture}`}
+							name={project.project_name}
+							add={false}
+							key={index}
+							onClick={() => {
+								window.scrollTo(0, 0);
+								Navigate(`/projects/${project.project_id}`);
+							}}
+						/>
+					))}
+				</div>
+			) : (
+				<EmptySection type={type} />
+			)}
+		</div>
+	);
+};
+
+export default Bookmarks;
--- src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
+++ src/components/DashBoard/StartupDashBoard/StartupDashBoard.jsx
@@ -16,93 +16,103 @@ import BarChart2 from "@/components/BarChart/BarChart2";
 import Likes from "@/components/Likes/Likes";
 import ProjectBox from "../ProjectBox/ProjectBox";
 import useProjectBoxStore from "@/stores/ProjectStore/ProjectBoxStore";
+import Bookmarks from "@/components/DashBoard/Bookmarks/Bookmarks";
 const StartupDashBoard = () => {
-  const {
-    username,
-    loading,
-    likeCount,
-    setLoading,
-    setFullname,
-    setLikeCount,
-    setEmail,
-    setPhone,
-    setUsername,
-    setBio,
-    setAvatar,
-    setHeader,
-    setBalance,
-  } = useProfileStore();
+	const {
+		username,
+		loading,
+		likeCount,
+		setLoading,
+		setFullname,
+		setLikeCount,
+		setEmail,
+		setPhone,
+		setUsername,
+		setBio,
+		setAvatar,
+		setHeader,
+		setBalance,
+	} = useProfileStore();
 
-  useEffect(() => {
-    setLoading(true);
-    getData(`/auth/view_own_baseuser_profile/`).then((data) => {
-      console.log("Startup data: ", data.base_profile);
-      setFullname(
-        data.base_profile.first_name + " " + data.base_profile.last_name
-      );
-      setUsername(data.base_profile.name);
-      setBio(data.base_profile.bio);
-      // if (data.base_profile.likeCount) {
-      // 	setLikeCount(data.base_profile.likeCount);
-      // } else {
-      // 	setLikeCount(0);
-      // }
-      setEmail(data.base_profile.email);
-      setPhone(data.base_profile.phone);
-      setAvatar(`http://104.168.46.4:8000${data.base_profile.profile_picture}`);
-      setHeader(`http://104.168.46.4:8000${data.base_profile.header_picture}`);
-      getData(`/balance/balance/`).then((data) => setBalance(data.balance));
-      getData(`/startup/get_startup_profile/${username}/`).then((d) => {
-        console.log("d: ", d.profile.id);
-        getData(`/startup/profile/${d.profile.id}/vote/`).then((data1) => {
-          console.log(data1.vote_count);
-          setLikeCount(data1.vote_count);
-        });
-      });
+	useEffect(() => {
+		setLoading(true);
+		getData(`/auth/view_own_baseuser_profile/`).then((data) => {
+			console.log("Startup data: ", data.base_profile);
+			setFullname(
+				data.base_profile.first_name + " " + data.base_profile.last_name
+			);
+			setUsername(data.base_profile.name);
+			setBio(data.base_profile.bio);
+			// if (data.base_profile.likeCount) {
+			// 	setLikeCount(data.base_profile.likeCount);
+			// } else {
+			// 	setLikeCount(0);
+			// }
+			setEmail(data.base_profile.email);
+			setPhone(data.base_profile.phone);
+			setAvatar(
+				`http://104.168.46.4:8000${data.base_profile.profile_picture}`
+			);
+			setHeader(
+				`http://104.168.46.4:8000${data.base_profile.header_picture}`
+			);
+			getData(`/balance/balance/`).then((data) =>
+				setBalance(data.balance)
+			);
+			getData(`/startup/get_startup_profile/${username}/`).then((d) => {
+				console.log("d: ", d.profile.id);
+				getData(`/startup/profile/${d.profile.id}/vote/`).then(
+					(data1) => {
+						console.log(data1.vote_count);
+						setLikeCount(data1.vote_count);
+					}
+				);
+			});
 
-      setLoading(false);
-    });
-  }, []);
-  if (loading) return <Loading />;
+			setLoading(false);
+		});
+	}, []);
+	if (loading) return <Loading />;
 
-  return (
-    <>
-      <Card className={styles.card_style}>
-        <Likes
-          className="translate-x-[1vw] translate-y-[11.5vw]"
-          count={likeCount}
-        />
-        <PersonalInfo loading={loading} />
-        <Label className={styles.label_style}>پوزیشن‌ها</Label>
-        <PositionBox />
-        <div className="flex flex-row justify-between gap-2 mt-2">
-          <div className="flex flex-col w-2/6 gap-2">
-            <Label className={styles.label_style}>حساب</Label>
-            <Accounting />
-          </div>
-          <div className="flex flex-col w-4/6 gap-2">
-            <Label className={styles.label_style}>اعضا</Label>
-            <TeamBox />
-          </div>
-        </div>
-        <Label className={styles.label_style}>پروژه‌ها</Label>
-        <ProjectBox type="پروژه‌" add={true} />
-        <Label className={styles.label_style}>آمار</Label>
-        <div
-          className={`flex flex-wrap gap-6 p-6 rounded-md ${styles.chartbox}`}
-        >
-          <div className="flex-1 min-w-[300px]">
-            <BarChart1 />
-          </div>
-          <div className="flex-1 min-w-[300px]">
-            <BarChart2 />
-          </div>
-        </div>
-        <Label className={styles.label_style}>ذخیره شده</Label>
-        <ProjectBox type="پروژه‌" />
-      </Card>
-    </>
-  );
+	return (
+		<>
+			<Card className={styles.card_style}>
+				<Likes
+					className="translate-x-[1vw] translate-y-[11.5vw]"
+					count={likeCount}
+				/>
+				<PersonalInfo loading={loading} />
+				<Label className={styles.label_style}>پوزیشن‌ها</Label>
+				<PositionBox />
+				<div className="flex flex-row justify-between gap-2 mt-2">
+					<div className="flex flex-col w-2/6 gap-2">
+						<Label className={styles.label_style}>حساب</Label>
+						<Accounting />
+					</div>
+					<div className="flex flex-col w-4/6 gap-2">
+						<Label className={styles.label_style}>اعضا</Label>
+						<TeamBox />
+					</div>
+				</div>
+				<Label className={styles.label_style}>پروژه‌ها</Label>
+				<ProjectBox type="پروژه‌" add={true} />
+				<Label className={styles.label_style}>آمار</Label>
+				<div
+					className={`flex flex-wrap gap-6 p-6 rounded-md ${styles.chartbox}`}
+				>
+					<div className="flex-1 min-w-[300px]">
+						<BarChart1 />
+					</div>
+					<div className="flex-1 min-w-[300px]">
+						<BarChart2 />
+					</div>
+				</div>
+				<Label className={styles.label_style}>ذخیره شده</Label>
+				{/* <ProjectBox type="پروژه‌" /> */}
+				<Bookmarks />
+			</Card>
+		</>
+	);
 };
 
 export default StartupDashBoard;
