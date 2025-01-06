37878891436b292b163941ee12d9a8039b1636fb
Amin <afiroozi007@gmail.com>
2025-01-06 20:31:38 +0330

    fix(profile): user type based handling

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -4,16 +4,22 @@ import StartupProfile from "@/components/Profile/StartupProfile/StartupProfile";
 import InvestorProfile from "@/components/Profile/InvestorProfile/InvestorProfile";
 import { useParams } from "react-router-dom";
 import { getData } from "@/Services/ApiClient/Services";
+import { useEffect, useState } from "react";
 
 const Profile = () => {
 	const { username } = useParams();
-	getData(`/auth/baseuser_search_by_name/${username}/`).then((data) => {
-		console.log(data);
-	});
-	console.log("mamad");
-	switch ("mamad") {
-		// case "startup":
-		case "mamad":
+	const [type, setType] = useState(null);
+	useEffect(() => {
+		getData(`/auth/baseuser_search_by_name/${username}/`).then((data) => {
+			console.log(data.baseuser_profile.user_type);
+			setType(data.baseuser_profile.user_type);
+		});
+		}, []);
+
+	// switch ("mamad") {
+	switch (type) {
+		case "startup":
+			// case "mamad":
 			return <StartupProfile />;
 		case "basic":
 			return <InvestorProfile />;
