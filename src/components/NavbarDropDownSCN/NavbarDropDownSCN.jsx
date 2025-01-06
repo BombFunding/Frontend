4a86c0646cb41cf0c55cf0c26bebc3732e617551
Amin <afiroozi007@gmail.com>
2025-01-06 17:28:24 +0330

    fix(navbarDropdown): navigation and hover

--- src/components/NavbarDropDownSCN/NavbarDropDownSCN.jsx
+++ src/components/NavbarDropDownSCN/NavbarDropDownSCN.jsx
@@ -113,8 +113,8 @@ const components = [
 // }
 
 function NavbarDropDownSCN() {
-  const Navigate = useNavigate();
-  const {
+	const Navigate = useNavigate();
+	const {
 		searchQuery,
 		sorting,
 		resultsPerPage,
@@ -128,87 +128,96 @@ function NavbarDropDownSCN() {
 		setSubcategory,
 		reset,
 	} = useStarboardStore();
-  const [selected, setSelected] = React.useState(null);
-  const [isHovering, setIsHovering] = React.useState(false);
-  //   console.log(components);
-  const handleMouseEnter = (index) => {
-    setSelected(index);
-    setIsHovering(true);
-  };
-  const searchProject = (category, subcategory) => {
+	const [selected, setSelected] = React.useState(null);
+	const [isHovering, setIsHovering] = React.useState(false);
+	//   console.log(components);
+	const handleMouseEnter = (index) => {
+		setSelected(index);
+		setIsHovering(true);
+	};
+	const searchProject = (category, subcategory) => {
 		console.log(category, subcategory);
 		setMainCategory(category);
 		setSubcategory(subcategory);
 	};
-  const handleMouseLeave = () => {
-    setIsHovering(false);
-    setTimeout(() => setSelected(null), 500); // Delay hiding for smooth opacity transition
-  };
-  const Dropdown = ({ i, comp }) => (
-    <div
-      className={`p-2 container rtl overflow-hidden grid text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
-        selected === i ? `${isHovering ? "unfader" : "opacity-0"}` : "hidden"
-      }`}
-    >
-      {Object.entries(comp.subs).map(([label, link], index) => (
-        <a
-          key={index}
-          href={link}
-          className="text-white hover:bg-gray-700 p-2 rounded-md"
-        >
-          {label}
-        </a>
-      ))}
-    </div>
-  );
-  return (
-    <div className="w-screen -translate-y-2 h-7 mt-6 grid grid-cols-6 px-[9vw] gap-[1.5vw] sm:text-[1.6vw] md:text-[1.5vw] lg:text-[1.2vw] font-vazirmatn">
-      {components.map((comp, i) => {
-        return (
-          <div
-            key={i}
-            className="place-items-center"
-            onMouseEnter={() => setSelected(i)}
-            // onMouseEnter={() => handleMouseEnter()}
-            onMouseLeave={() => setSelected(null)}
-            // onMouseLeave={() => handleMouseLeave()}
-          >
-            <div className="hover:cursor-pointer place-content-center h-fit ml-[2.5vw] px-[1vw] w-[12vw] flex  flex-row text-black">
-              {comp.title}
-              <FiChevronDown
-                className={`mt-1.5 ml-[0.2vw] transition ${
-                  selected == i ? "rotate-180  duration-500" : ""
-                }`}
-              />
-            </div>
-            {/* <Dropdown i={i} comp={comp} /> */}
-            <div
-              className={`p-2 container rtl overflow-hidden grid grid-cols-2 gap-2 auto-rows-auto text-[1vw] text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
-                selected === i
-                  ? "unfader pointer-events-auto "
-                  : "fader pointer-events-none"
-              }`}
-            >
-              {Object.entries(comp.subs).map(([label, link], index) => (
-                <a
-                  key={index}
-                  href={link}
-                  className="text-white hover:bg-gray-700 p-2 rounded-md"
-                  onClick={() => {
-										console.log("label", comp.title, label);
-										searchProject(comp.title, label);
-										Navigate("/starboard");
-									}}
-                >
-                  {label}
-                </a>
-              ))}
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+	const handleMouseLeave = () => {
+		setIsHovering(false);
+		setTimeout(() => setSelected(null), 500); // Delay hiding for smooth opacity transition
+	};
+	const Dropdown = ({ i, comp }) => (
+		<div
+			className={`p-2 container rtl overflow-hidden grid text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
+				selected === i
+					? `${isHovering ? "unfader" : "opacity-0"}`
+					: "hidden"
+			}`}
+		>
+			{Object.entries(comp.subs).map(([label, link], index) => (
+				<a
+					key={index}
+					href={link}
+					className="text-white hover:bg-gray-700 p-2 rounded-md"
+				>
+					{label}
+				</a>
+			))}
+		</div>
+	);
+	return (
+		<div className="w-screen -translate-y-2 h-7 mt-6 grid grid-cols-6 px-[9vw] gap-[1.5vw] sm:text-[1.6vw] md:text-[1.5vw] lg:text-[1.2vw] font-vazirmatn">
+			{components.map((comp, i) => {
+				return (
+					<div
+						key={i}
+						className="place-items-center"
+						onMouseEnter={() => setSelected(i)}
+						// onMouseEnter={() => handleMouseEnter()}
+						onMouseLeave={() => setSelected(null)}
+						// onMouseLeave={() => handleMouseLeave()}
+					>
+						<div className="hover:cursor-pointer place-content-center h-fit ml-[2.5vw] px-[1vw] w-[12vw] flex  flex-row text-black">
+							{comp.title}
+							<FiChevronDown
+								className={`mt-1.5 ml-[0.2vw] transition ${
+									selected == i
+										? "rotate-180  duration-500"
+										: ""
+								}`}
+							/>
+						</div>
+						{/* <Dropdown i={i} comp={comp} /> */}
+						<div
+							className={`p-2 container rtl overflow-hidden grid grid-cols-2 gap-2 auto-rows-auto text-[1vw] text-center w-[15vw] bg-gray-800 mt-1 rounded-md sm:-translate-x-[1vw] md:-translate-x-[0vw] ${
+								selected === i
+									? "unfader pointer-events-auto "
+									: "fader pointer-events-none"
+							}`}
+						>
+							{Object.entries(comp.subs).map(
+								([label, link], index) => (
+									<a
+										key={index}
+										className="text-white hover:bg-gray-700 p-2 rounded-md hover:cursor-pointer"
+										onClick={() => {
+											console.log(
+												"label",
+												comp.title,
+												label
+											);
+											searchProject(comp.title, label);
+											Navigate("/starboard");
+										}}
+									>
+										{label}
+									</a>
+								)
+							)}
+						</div>
+					</div>
+				);
+			})}
+		</div>
+	);
 }
 
 export default NavbarDropDownSCN;
