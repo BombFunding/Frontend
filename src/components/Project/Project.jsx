c3d3e70b5b452a7e79f864841bfc026b64f9405f
erfan._.teikst <erfan.teist2027@gmail.com>
2025-01-06 18:52:26 +0330

    fix: publicProject responsive

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -254,7 +254,7 @@ function Project({ className }) {
 				<Like />
 				<Bookmark />
 			</div> */}
-      <div className="flex">
+      <div className="flex flex-col-reverse lg:flex-row">
         <Tags
           tags={subcategories.map(
             (subcategory) => englishToPersian[subcategory] || subcategory
@@ -267,9 +267,9 @@ function Project({ className }) {
         ) : (
           <></>
         )}
-        <div className="my-[2vw] flex place-items-center bg-red-600">
+        <div className="my-[2vw] flex place-items-center w-full justify-end">
           <div className="flex rtl gap-[0.5vw] place-self-start px-[2vw] py-[1vh]">
-            <div className="text-black text-4xl place-self-center">
+            <div className="text-black text-3xl place-self-center">
               {totalFunded}
             </div>
             <img
