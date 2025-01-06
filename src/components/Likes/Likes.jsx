a6dfe037e0dfd0030ae44192222047d84490c57f
Amin <afiroozi007@gmail.com>
2025-01-06 20:21:01 +0330

    fix(likes): responsive

--- src/components/Likes/Likes.jsx
+++ src/components/Likes/Likes.jsx
@@ -4,7 +4,7 @@ function Likes({ count, className }) {
 		<button className={`${styles.Btn} ${className}`}>
 			<span className={`${styles.likeCount}`}>{count}</span>
 			<span className={`${styles.leftContainer}`}>
-				<svg viewBox="0 0 24 24" fill="white" className="lg:w-[36px] md:w-[32px] sm:w-[28px] w-[24px] mt-[2px] mr-[5px]">
+				<svg viewBox="0 0 24 24" fill="white" className="lg:w-[24px] md:w-[20px] sm:w-[20px] w-[24px] mt-[2px] mr-[5px]">
 					<g id="SVGRepo_iconCarrier">
 						<path d="M20.808,11.079C19.829,16.132,12,20.5,12,20.5s-7.829-4.368-8.808-9.421C2.227,6.1,5.066,3.5,8,3.5a4.444,4.444,0,0,1,4,2,4.444,4.444,0,0,1,4-2C18.934,3.5,21.773,6.1,20.808,11.079Z"></path>
 					</g>
