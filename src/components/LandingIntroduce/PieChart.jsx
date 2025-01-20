59e1b56bd3583b4d1c8061703ad477f11acab22a
taraneh <152805258+taraneh2abd@users.noreply.github.com>
2025-01-21 03:24:03 +0330

    fix (piechart) : changed texts

--- src/components/LandingIntroduce/PieChart.jsx
+++ src/components/LandingIntroduce/PieChart.jsx
@@ -152,140 +152,158 @@ const PieChartComponent = () => {
   );
 };
 
+const PieChartComponent2 = () => {
+  const [chartData, setChartData] = React.useState([]); 
 
-// const PieChartComponent2 = () => {
-//   const chartData = React.useMemo(() => getRandomData(), []); 
-//   const totalVisitors = React.useMemo(() => {
-//     return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
-//   }, [chartData]);
+  React.useEffect(() => {
+    
+    const fetchData = async () => {
+      const data = await getRandomData();
+      console.log("Chart Data:", data); 
+      setChartData(data); 
+    };
 
-//   const { ref, inView } = useInView({
-//     triggerOnce: true, 
-//     threshold: 1, 
-//   });
+    fetchData();
+  }, []); 
 
-//   return (
-//     <Card
-//       ref={ref}
-//       className="flex flex-col" 
-//     >
-//       <CardHeader className="items-center pb-0">
-//         <CardTitle>توزیع محبوبیت استارتاپ ها</CardTitle>
-//         <CardDescription>January - June 2024</CardDescription>
-//       </CardHeader>
-//       <CardContent className="flex-1 pb-0">
-//         <ChartContainer className="mx-auto aspect-square max-h-[250px]">
-//           <PieChart>
-//             <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-//             <Pie
-//               data={chartData}
-//               dataKey="visitors"
-//               nameKey="browser"
-//               innerRadius={60}
-//               strokeWidth={5}
-//               isAnimationActive={inView} 
-//             >
-//               <Label
-//                 content={({ viewBox }) => {
-//                   if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-//                     return (
-//                       <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
-//                         <tspan x={viewBox.cx} y={viewBox.cy} className="fill-foreground text-3xl font-bold">
-//                           {totalVisitors.toLocaleString()}
-//                         </tspan>
-//                         <tspan x={viewBox.cx} y={(viewBox.cy || 0) + 24} className="fill-muted-foreground">
-//                           Visitors
-//                         </tspan>
-//                       </text>
-//                     );
-//                   }
-//                 }}
-//               />
-//             </Pie>
-//           </PieChart>
-//         </ChartContainer>
-//       </CardContent>
-//       <CardFooter className="flex-col gap-2 text-sm">
-//         <div className="flex items-center gap-2 font-medium leading-none">
-//           مجموع درآمد همه استارتاپ ها در یک ماه گذشته<TrendingUp className="h-4 w-4" />
-//         </div>
-//         <div className="leading-none text-muted-foreground">
-// توزیع درآمد ها برحسب دسته بندی پروژه است
-//         </div>
-//       </CardFooter>
-//     </Card>
-//   );
-// };
+  const totalVisitors = React.useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+  }, [chartData]);
 
-// const PieChartComponent3 = () => {
-//   const chartData = React.useMemo(() => getRandomData(), []); 
-//   const totalVisitors = React.useMemo(() => {
-//     return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
-//   }, [chartData]);
+  const { ref, inView } = useInView({
+    triggerOnce: true, 
+    threshold: 1, 
+  });
 
-//   const { ref, inView } = useInView({
-//     triggerOnce: true, 
-//     threshold: 1, 
-//   });
+  return (
+    <Card ref={ref} className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>توزیع محبوبیت استارتاپ ها</CardTitle>
+                <CardDescription>{`${lastMonthName} - ${persianMonthName} ${persianYear}`}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer className="mx-auto aspect-square max-h-[250px]">
+          <PieChart>
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+              isAnimationActive={inView}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
+                        <tspan x={viewBox.cx} y={viewBox.cy} className="fill-foreground text-3xl font-bold">
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan x={viewBox.cx} y={(viewBox.cy || 0) + 24} className="fill-muted-foreground">
+                          هزار تومان
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          مجموع درآمد همه استارتاپ ها در یک ماه گذشته<TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+        مقادیر برحسب هزار تومان است
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
 
-//   return (
-//     <Card
-//       ref={ref}
-//       className="flex flex-col" 
-//     >
-//       <CardHeader className="items-center pb-0">
-//         <CardTitle>توزیع بازدید استارتاپ ها</CardTitle>
-//         <CardDescription>January - June 2024</CardDescription>
-//       </CardHeader>
-//       <CardContent className="flex-1 pb-0">
-//         <ChartContainer className="mx-auto aspect-square max-h-[250px]">
-//           <PieChart>
-//             <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
-//             <Pie
-//               data={chartData}
-//               dataKey="visitors"
-//               nameKey="browser"
-//               innerRadius={60}
-//               strokeWidth={5}
-//               isAnimationActive={inView} 
-//             >
-//               <Label
-//                 content={({ viewBox }) => {
-//                   if (viewBox && "cx" in viewBox && "cy" in viewBox) {
-//                     return (
-//                       <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
-//                         <tspan x={viewBox.cx} y={viewBox.cy} className="fill-foreground text-3xl font-bold">
-//                           {totalVisitors.toLocaleString()}
-//                         </tspan>
-//                         <tspan x={viewBox.cx} y={(viewBox.cy || 0) + 24} className="fill-muted-foreground">
-//                           بازدید
-//                         </tspan>
-//                       </text>
-//                     );
-//                   }
-//                 }}
-//               />
-//             </Pie>
-//           </PieChart>
-//         </ChartContainer>
-//       </CardContent>
-//       <CardFooter className="flex-col gap-2 text-sm">
-//         <div className="flex items-center gap-2 font-medium leading-none">
-//           مجموع درآمد همه استارتاپ ها در یک ماه گذشته<TrendingUp className="h-4 w-4" />
-//         </div>
-//         <div className="leading-none text-muted-foreground">
-// توزیع درآمد ها برحسب دسته بندی پروژه است
-//         </div>
-//       </CardFooter>
-//     </Card>
-//   );
-// };
+const PieChartComponent3 = () => {
+  const [chartData, setChartData] = React.useState([]); 
+
+  React.useEffect(() => {
+    
+    const fetchData = async () => {
+      const data = await getRandomData();
+      console.log("Chart Data:", data); 
+      setChartData(data); 
+    };
+
+    fetchData();
+  }, []); 
+
+  const totalVisitors = React.useMemo(() => {
+    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+  }, [chartData]);
+
+  const { ref, inView } = useInView({
+    triggerOnce: true, 
+    threshold: 1, 
+  });
+
+  return (
+    <Card ref={ref} className="flex flex-col">
+      <CardHeader className="items-center pb-0">
+        <CardTitle>توزیع بازدید استارتاپ ها</CardTitle>
+                <CardDescription>{`${lastMonthName} - ${persianMonthName} ${persianYear}`}</CardDescription>
+      </CardHeader>
+      <CardContent className="flex-1 pb-0">
+        <ChartContainer className="mx-auto aspect-square max-h-[250px]">
+          <PieChart>
+            <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+            <Pie
+              data={chartData}
+              dataKey="visitors"
+              nameKey="browser"
+              innerRadius={60}
+              strokeWidth={5}
+              isAnimationActive={inView}
+            >
+              <Label
+                content={({ viewBox }) => {
+                  if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                    return (
+                      <text x={viewBox.cx} y={viewBox.cy} textAnchor="middle" dominantBaseline="middle">
+                        <tspan x={viewBox.cx} y={viewBox.cy} className="fill-foreground text-3xl font-bold">
+                          {totalVisitors.toLocaleString()}
+                        </tspan>
+                        <tspan x={viewBox.cx} y={(viewBox.cy || 0) + 24} className="fill-muted-foreground">
+                          هزار تومان
+                        </tspan>
+                      </text>
+                    );
+                  }
+                }}
+              />
+            </Pie>
+          </PieChart>
+        </ChartContainer>
+      </CardContent>
+      <CardFooter className="flex-col gap-2 text-sm">
+        <div className="flex items-center gap-2 font-medium leading-none">
+          مجموع درآمد همه استارتاپ ها در یک ماه گذشته<TrendingUp className="h-4 w-4" />
+        </div>
+        <div className="leading-none text-muted-foreground">
+        مقادیر برحسب هزار تومان است
+        </div>
+      </CardFooter>
+    </Card>
+  );
+};
 
 function ThreePieCharts() {
   return (
 <div className="flex flex-wrap lg:flex-nowrap flex-col lg:flex-row justify-around items-center gap-4">
-  {/* <PieChartComponent3 />
-  <PieChartComponent2 /> */}
+  <PieChartComponent3 />
+  <PieChartComponent2 />
+  <PieChartComponent />
   <PieChartComponent />
 </div>
 
