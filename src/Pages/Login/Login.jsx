7b8921fbbc668f72f226e11924ff5b8b108aa163
MiliAxe <miliaxe0@gmail.com>
2025-01-25 21:32:23 +0330

    fix(Login): center login container and adjust width for better layout

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,11 +6,13 @@ import HomeButton from "@/components/HomeButton/HomeButton";
 function Login() {
   return (
     <>
-      <div className={styles.login_container}>
-        <img className={styles.bg_img} src={loginimage} alt="login" />
-        <LoginForm />
+      <div className="flex justify-center w-screen">
+        <div className={styles.login_container}>
+          <img className={styles.bg_img} src={loginimage} alt="login" />
+          <LoginForm />
+        </div>
+        <HomeButton className="top-12 right-10 lg:right-24" />
       </div>
-      <HomeButton className="top-12 right-10 lg:right-24" />
     </>
   );
 }
