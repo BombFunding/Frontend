b9aa77d7d058c4f0a14385e6c62aad097a52d785
Amin <afiroozi007@gmail.com>
2025-01-26 00:57:36 +0330

    fix(profile): handle no team member

--- src/components/DashBoard/ProjectItem/ProjectItem.jsx
+++ src/components/DashBoard/ProjectItem/ProjectItem.jsx
@@ -3,36 +3,43 @@ import styles from "./ProjectItem.module.scss";
 import { Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const ProjectItem = ({ header, name, add, className, onClick }) => {
-  if (add)
-    return (
-      <>
-        <AddProjectForm
-          addProjectCard={
-            <Card
-              className={`rounded-lg shadow-lg w-[30vw] h-full place-content-center text-gray-400 bg-gray-200 hover:cursor-pointer min-h-[250px] ${className}`}
-            >
-              <Label
-                className={`text-[2vw] place-self-center place-content-center place-items-center flex flex-col h-full min-h-14`}
-              >
-                <p>+</p>
-                <p>پروژه جدید</p>
-              </Label>
-            </Card>
-          }
-        />
-      </>
-    );
-  return (
-    <Card
-      onClick={onClick}
-      className={`rounded-lg shadow-lg w-[30vw] hover:cursor-pointer min-h-14 ${className}`}
-    >
-      <img src={header} className={styles.image} />
-      <Label className={styles.text}>{name}</Label>
-    </Card>
-  );
+	const [open, setOpen] = useState(false);
+	if (add)
+		return (
+			<>
+				<AddProjectForm
+        setOpen={setOpen}
+        open={open}
+					addProjectCard={
+						<Card
+							onClick={() => {
+								setOpen(true);
+							}}
+							className={`rounded-lg shadow-lg w-[30vw] h-full place-content-center text-gray-400 bg-gray-200 hover:cursor-pointer min-h-[250px] ${className}`}
+						>
+							<Label
+								className={`text-[2vw] place-self-center place-content-center place-items-center flex flex-col h-full min-h-14 hover:cursor-pointer`}
+							>
+								<p>+</p>
+								<p>پروژه جدید</p>
+							</Label>
+						</Card>
+					}
+				/>
+			</>
+		);
+	return (
+		<Card
+			onClick={onClick}
+			className={`rounded-lg shadow-lg w-[30vw] hover:cursor-pointer min-h-14 ${className}`}
+		>
+			<img src={header} className={styles.image} />
+			<Label className={styles.text}>{name}</Label>
+		</Card>
+	);
 };
 
 export default ProjectItem;
